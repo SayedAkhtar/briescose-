cba364773746a72b4df8ac50db11afda8ed733dd
sayed <sayed182@gmail.com>
2021-01-31 13:39:08 +0530

    Added responsiveness somewhat

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 // Variables
 var megaList = document.querySelectorAll('.nav-menu--list');
 var megaSubList = document.querySelectorAll('.megaMenuTab-nav-category li a');
+var mobileMenuOpen = document.querySelector('#mobile-menu-toggle');
+var mobileMenuClose = document.querySelector('#closeMenuMobile');
+var mobileMenuNav = document.querySelector("#slidingMenuMobile-wapper .slider-menu__container");
+var mobileMainUL = document.querySelector('.slider-menu__menu');
+var mobileMainLI= document.querySelectorAll('.slider-menu__item.has-sub');
+var mobileSubMenuBackBtn = document.querySelector('#slidingMenuMobile-wapper .slider-menu__main');
 
 
 megaList.forEach((element) => {
@@ -26,14 +32,42 @@ megaSubList.forEach((element) => {
         var selectedId = event.target.dataset.id;
         removeClassFromElement(document.querySelectorAll('.megaMenuTab-colRight .tab-panel'));
         var toBeDisplayed = document.querySelector(`.sub-container.active .megaMenuTab-colRight #${selectedId}`);
-        console.log(toBeDisplayed);
         toBeDisplayed.classList.add('active');
     });
 
-    element.addEventListener('mouseleave', (event)=>{
-        removeClassFromElement(document.querySelectorAll('.megaMenuTab-colRight .tab-panel'));
-    });
+    // element.addEventListener('mouseleave', (event)=>{
+    //     removeClassFromElement(document.querySelectorAll('.megaMenuTab-colRight .tab-panel'));
+    // });
     
+});
+
+
+mobileMenuOpen.addEventListener('click',toggleMenu);
+
+mobileMenuClose.addEventListener('click', toggleMenu);
+
+document.querySelector('.slider-menu__main').addEventListener('click',(event)=>{
+    if(!event.target.classList.contains('active'))
+    return;
+    mobileMenuNav.style.left= '0%';
+    event.target.classList.remove('active');
+});
+
+
+
+mobileMainLI.forEach((element)=>{
+    element.addEventListener('click', (event)=>{
+        mobileMenuNav.style.left = '-100%';
+        event.target.classList.add('show');
+        console.log(event.target.childNodes);
+        try{
+            mobileSubMenuBackBtn.classList.add('active');
+            event.target.childNodes[3].classList.add('slider-menu--active');
+            
+        }catch{
+            console.log("no child present");
+        }
+    });
 });
 
 function removeClassFromElement(element){
@@ -49,6 +83,21 @@ function removeClassFromElement(element){
         return false;
     }
 }
+
+function toggleMenu(){
+    var isOpen = document.querySelector('#slidingMenuMobile-wapper').classList.contains('show');
+    if(!isOpen){
+        document.querySelector('#slidingMenuMobile-wapper').classList.add('show');
+    }else{
+        document.querySelector('#slidingMenuMobile-wapper').classList.remove('show');
+    }
+}
+
+
+
+
+
+
 
 
 let imageLinksArray = [
@@ -69,11 +118,24 @@ function propagateProductsCard(parent, times){
             productCardClone.childNodes[1].childNodes[1].childNodes[1].childNodes[1].src = imageLinksArray[i];
         } catch (error) {
             console.error("Wrong/Changed formating of Product card");
+            productCardClone.childNodes[1].childNodes[1].childNodes[1].childNodes[1].src = 'https://via.placeholder.com/300x300';
+            
         }
-        
-        console.log("printing "+i);
         document.querySelector('.productList .row').appendChild(productCardClone); 
     }
 }
 
-propagateProductsCard(document.querySelector('.productCardColumn'),8);
+// propagateProductsCard(document.querySelector('.productCardColumn'),imageLinksArray.length);
+
+
+arr = [];
+document.querySelectorAll('.sub-nav .container span').forEach((element) => {
+    arr.push(element.innerText);
+})
+i = 0
+setInterval(()=> {
+    if(i >= arr.length){i = 0;}
+    document.querySelector('.sub-nav .container p').innerText = arr[i];
+    i++;
+    
+}, 1000)
